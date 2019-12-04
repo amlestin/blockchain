ab657cd67c69d0a4ad105b847c18bd2eebbc75ee
Arnold Lestin <amlestin@gmail.com>
2019-12-04 13:29:06 -0500

    Removed import that prevented install Python27

--- blockchain-workbench/rest-api-samples/python/swagger_client/models/workflow_data_type.py
+++ blockchain-workbench/rest-api-samples/python/swagger_client/models/workflow_data_type.py
@@ -16,7 +16,7 @@ import re  # noqa: F401
 
 import six
 
-from swagger_client.models.workflow_data_type import WorkflowDataType  # noqa: F401,E501
+#from swagger_client.models.workflow_data_type import WorkflowDataType  # noqa: F401,E501
 
 
 class WorkflowDataType(object):
